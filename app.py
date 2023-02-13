9125f09230579fad3aaefcdf5482e1a933297b1a
devskr <87925081+devskr@users.noreply.github.com>
2023-02-13 20:15:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from eth_account import Account
 app = Flask(__name__)
 
 # Connect to the FTM network
-w3 = Web3(Web3.HTTPProvider("https://rpc.ankr.com/fantom/"))
+w3 = Web3(Web3.HTTPProvider("https://mainnet-rpc.thundercore.com/"))
 w3.middleware_onion.inject(geth_poa_middleware, layer=0)
 abi = json.loads('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"spender","type":"address"},{"name":"tokens","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"from","type":"address"},{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"_totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeSub","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":false,"inputs":[{"name":"to","type":"address"},{"name":"tokens","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeDiv","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeMul","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"constant":true,"inputs":[{"name":"tokenOwner","type":"address"},{"name":"spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"a","type":"uint256"},{"name":"b","type":"uint256"}],"name":"safeAdd","outputs":[{"name":"c","type":"uint256"}],"payable":false,"stateMutability":"pure","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"tokenOwner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"tokens","type":"uint256"}],"name":"Approval","type":"event"}]')
 
@@ -32,7 +32,7 @@ def send_tokens():
          totalSupply = contract.functions.totalSupply().call()
          amo = w3.toWei(amount, 'ether')
          tx = contract.functions.transfer(recipient_address, amo).buildTransaction({
-             'chainId':250, 
+             'chainId':108, 
              'gas': gas_amount,
              'gasPrice': gas_price,
              'nonce':nonce})
@@ -64,7 +64,7 @@ def send_coin():
         nonce = w3.eth.getTransactionCount(saddress)
         amo = w3.toWei(value, 'ether')
         gas_price = w3.eth.gasPrice
-        tx = {'nonce' : nonce,'to' : to,'value' : amo, 'chainId':250, 'gas' : 21000,'gasPrice' : gas_price}
+        tx = {'nonce' : nonce,'to' : to,'value' : amo, 'chainId':108, 'gas' : 21000,'gasPrice' : gas_price}
         sign_tx = w3.eth.account.signTransaction(tx, privateKey)
         tran_hash = w3.eth.sendRawTransaction(sign_tx.rawTransaction)
         txn = w3.toHex(tran_hash)
@@ -76,3 +76,4 @@ def send_coin():
 @app.route('/')
 def setuphandler():
  return ("This is polygon token and coin api to know how to use dm @ChauhanDev07 on telegram ")
+https://mainnet-rpc.thundercore.com
